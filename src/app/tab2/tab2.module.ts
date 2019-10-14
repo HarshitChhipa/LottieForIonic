5433c90356a37b79eca98682c3c7efb7b1026363
HarshitChhipa <harshitkankerwal@gmail.com>
2019-10-14 23:19:47 +0530

    Added lottie animation of walking man

--- src/app/tab2/tab2.module.ts
+++ src/app/tab2/tab2.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Tab2Page } from './tab2.page';
+import { LottieAnimationViewModule } from 'ng-lottie';
 
 @NgModule({
   imports: [
     IonicModule,
     CommonModule,
     FormsModule,
+    LottieAnimationViewModule.forRoot(),
     RouterModule.forChild([{ path: '', component: Tab2Page }])
   ],
   declarations: [Tab2Page]
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -6,7 +6,37 @@ import { Component } from '@angular/core';
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  public lottieConfig: Object;
+  private anim: any;
+  private animationSpeed: number = 1;
 
-  constructor() {}
+  constructor() {
+    this.lottieConfig = {
+      path: 'https://assets6.lottiefiles.com/packages/lf20_9eLZ98.json',
+      renderer: 'canvas',
+      autoplay: true,
+      loop: true
+    };
+  }
 
+  handleAnimation(anim: any) {
+    this.anim = anim;
+  }
+
+  stop() {
+    this.anim.stop();
+  }
+
+  play() {
+    this.anim.play();
+  }
+
+  pause() {
+    this.anim.pause();
+  }
+
+  setSpeed(speed: number) {
+    this.animationSpeed = speed;
+    this.anim.setSpeed(speed);
+  }
 }
